d302e63a6b3463dc651cafbde27881a8880bb12e
Luis Martin MV <l20121013@morelia.tecnm.mx>
2023-05-26 18:25:55 -0600

    Creacion del loguin

--- PHP/loguin.php
+++ PHP/loguin.php
@@ -9,9 +9,43 @@
     <title>Bookmania:inicio de sesion</title>
 </head>
 <body>
-    <h1>Hola estoy probando </h1>
+    <form action="">
+    <div class="wrapper">
+        <div class="container main">
+            <div class="row">
+                <div class="col-md-6 side-image">
+                    <!-------Image-------->
+                    <img src="" alt="">
+                    <div class="text">
+                        <p>"La lectura abre puertas hacia mundos infinitos"<i></i></p>
+                    </div>
+                </div>
+                <div class="col-md-6 right">
+                     <div class="input-box">
+                        <header>Iniciar sesion</header>
+                        <div class="input-field">
+                            <input type="text" class="input" id="correo" required autocomplete="off">
+                            <label for="correo">Correo</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="password" class="input" id="password" required>
+                            <label for="password">Contraseña</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="submit" class="submit" value="iniciar sesion">
+                            
+                        </div>
+                        <div class="signin">
+                            <span>¿Ya tienes una cuenta? <a href="#">crea una ahora</a></span>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
 
 
-    <script src="../JS/bootstrap.min.js"></script>r    
+    <script src="../JS/bootstrap.min.js"></script>   
 </body>
 </html>
